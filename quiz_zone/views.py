4809d888b8c0945a1131c4a0141ce5b627d9889a
Rukaiya <rukaiyazaman9999@gmail.com>
2024-10-06 22:14:54 +0800

    final update

--- quiz_zone/views.py
+++ quiz_zone/views.py
@@ -1,10 +1,2 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'registration.html')
-
-def Userlogin(request):
-    return render(request, 'login.html')
-
-def my_view(request):
-    return render(request, 'base.html')
