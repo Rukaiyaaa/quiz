acfbf00dc161eb5a884f6face8b199afe831486d
Rukaiya <rukaiyazaman9999@gmail.com>
2024-10-07 10:49:50 +0800

    code

--- quizes/views.py
+++ quizes/views.py
@@ -2,10 +2,10 @@ from rest_framework import viewsets, pagination, filters
 from .models import Quiz
 from .serializers import QuizSerializer
 
-class QuizPagination(pagination.PageNumberPagination):
-    page_size = 1
-    page_size_query_param = 'page_size'
-    max_page_size = 100
+# class QuizPagination(pagination.PageNumberPagination):
+#     page_size = 1
+#     page_size_query_param = 'page_size'
+#     max_page_size = 100
 
 
 class QuizViewSet(viewsets.ModelViewSet):
@@ -14,5 +14,5 @@ class QuizViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter]
     search_fields = ['topic__category', 'name']  
     filterset_fields = ['topic__category', 'name'] 
-    pagination_class = QuizPagination
+    # pagination_class = QuizPagination
     
