f95d758bcfe7568a1dfad1d1eea3629506bb861e
Rukaiya <rukaiyazaman9999@gmail.com>
2024-10-07 10:09:06 +0800

    views update

--- quiz_zone/settings.py
+++ quiz_zone/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
     'rest_framework',
+    'django_filters',
     'corsheaders',
     'account',
     'quizes',
--- quizes/views.py
+++ quizes/views.py
@@ -1,9 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, pagination, filters
 from .models import Quiz
 from .serializers import QuizSerializer
+
+class QuizPagination(pagination.PageNumberPagination):
+    page_size = 1
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 
 class QuizViewSet(viewsets.ModelViewSet):
     queryset = Quiz.objects.all()
     serializer_class = QuizSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['topic__category', 'name']  
+    filterset_fields = ['topic__category', 'name'] 
+    pagination_class = QuizPagination
     
