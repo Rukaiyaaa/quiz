94677ff3ae3bf1832c504da0fbae61dbe337b221
Rukaiya <rukaiyazaman9999@gmail.com>
2024-10-07 12:36:29 +0800

    code

--- quizes/serializers.py
+++ quizes/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 from .models import Quiz
 from questions.serializers import QuestionSerializer
+from category.serializers import CategorySerializer
 
 class QuizSerializer(serializers.ModelSerializer):
     questions = QuestionSerializer(many=True, read_only=True, source='get_questions')
+
+    topic = CategorySerializer()
 
     class Meta:
         model = Quiz
